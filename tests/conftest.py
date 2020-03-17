13e2027a3811c215353d05acea1c2bff73206eae
Madalin Popa <contact@madalinpopa.com>
2020-03-17 19:09:55 +0200

    update test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,23 +4,39 @@
 # tests/conftest.py
 
 import pytest
+from sqlalchemy.orm import clear_mappers
+from sqlalchemy import create_engine
+
+from app import guard
 
 from app import create_app
-
-from app.database import engine, Session
+from app.database import Session
+from app.model import User
 from app.orm import metadata, start_mapper
+
 
 @pytest.fixture
 def client():
+
+    engine = create_engine("sqlite:///test.db")
+
     application = create_app()
-    application.testing = True
-    application.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-    client = application.test_client()
 
-    with application.app_context():
-        start_mapper()
-        metadata.create_all(bind=engine)
-    application.app_context().push()
+    with application.test_client() as client:
+        with application.app_context():
+            start_mapper()
+            metadata.create_all(bind=engine)
+            init_users()
+        yield client
 
-    yield client
+    clear_mappers()
     Session.remove()
+
+
+def init_users():
+
+    user1 = User("user1", guard.hash_password("secret1"))
+    user2 = User("user2", guard.hash_password("secret2"))
+    user3 = User("user3", guard.hash_password("secret3"))
+    Session.add_all([user1, user2, user3])
+    Session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,8 +2,15 @@
 
 # tests/test_routes.py
 
+from flask import json
 
 
-def test_smoke(client):
-    rv = client.get('/')
+def test_home(client):
+    rv = client.get("/")
     assert rv.status_code == 200
+
+
+#def test_login(client):
+#    data = {"username": "user1", "password": "secret1" }
+#    rv = client.post("/login", data=json.dumps(data), content_type="application/json")
+#    assert rv.status_code == 200
